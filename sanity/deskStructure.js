cedf2c685b7375415acfcbd0cc9f37764243806a
Chrish Dunne <hey@chrish.design>
2020-10-21 17:30:36 +0100

    [~] Quotey

--- sanity/deskStructure.js
+++ sanity/deskStructure.js
@@ -2,26 +2,24 @@ import S from '@sanity/desk-tool/structure-builder';
 
 export default () =>
   S.list()
-    .title('Content')
+    .title("Content")
     .items([
       // Homepage item
-      S.listItem().title('Landing').child(
-        S.editor()
-          .schemaType('page')
-          .documentId('homepage')
-      ),
+      S.listItem()
+        .title("Landing")
+        .child(S.editor().schemaType("page").documentId("homepage")),
       // Quiz item
       S.listItem()
-        .title('Quiz')
+        .title("Quiz")
         .child(
           S.list()
-            .title('Quiz')
+            .title("Quiz")
             .items([
-              S.listItem().title('Quiz Page'),
+              S.listItem().title("Quiz Page"),
               S.listItem()
-                .title('Questions')
-                .schemaType('quizQuestions')
-                .child(S.documentTypeList('quizQuestions')),
+                .title("Questions")
+                .schemaType("quizQuestions")
+                .child(S.documentTypeList("quizQuestions")),
             ])
         ),
     ]);
