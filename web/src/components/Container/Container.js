00270a62e02ff5231a23c671ced74ab375c76143
Chrish Dunne <hey@chrish.design>
2020-10-22 14:36:02 +0100

    [+] Added container

---
+++ web/src/components/Container/Container.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+//
+
+const breakpoints = [640, 768, 1024, 1280];
+
+const PageContainer = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 1rem;
+
+  ${breakpoints.map(
+    (bp) => `@media(min-width: ${bp / 16}rem) { max-width: ${bp / 16}rem; }`
+  )}
+`;
+
+const Container = ({ children, className }) => (
+  <PageContainer className={className}>{children}</PageContainer>
+);
+
+export default Container;
+
+//
+
+Container.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+};
+
+Container.defaultProps = {
+  className: null,
+};
