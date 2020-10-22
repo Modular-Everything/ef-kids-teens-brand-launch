0be771acae8623c89bff3546b03e6b9810042448
Chrish Dunne <hey@chrish.design>
2020-10-22 14:57:05 +0100

    [+] Added header

---
+++ web/src/components/Header/Header.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { Link } from 'gatsby';
+
+import Container from '../Container';
+import Logo from '../../assets/logo.svg';
+import ArrowLeft from '../../assets/icons/arrow-left.svg';
+
+//
+
+const Header = ({ title }) => (
+  <HeaderWrap>
+    <HeaderContainer>
+      <Return to="/">
+        <span>
+          <img src={ArrowLeft} alt="" />
+        </span>
+        Back to main site
+      </Return>
+
+      <LogoWrap to="/">
+        <img src={Logo} alt={title} />
+      </LogoWrap>
+    </HeaderContainer>
+  </HeaderWrap>
+);
+
+export default Header;
+
+//
+
+const HeaderWrap = styled.header`
+  background-color: #fff;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.15);
+`;
+
+const HeaderContainer = styled(Container)`
+  display: flex;
+  align-items: center;
+  height: 7rem;
+`;
+
+const LogoWrap = styled(Link)`
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, 0);
+`;
+
+const Return = styled(Link)`
+  display: none;
+  color: var(--ef-black);
+  text-decoration: none;
+
+  @media screen and (min-width: 768px) {
+    display: block;
+  }
+
+  & span {
+    display: inline-block;
+    width: 1rem;
+    margin-right: 1.5rem;
+
+    & img {
+      width: 100%;
+    }
+  }
+`;
+
+//
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
--- web/src/components/Layout/Layout.js
+++ web/src/components/Layout/Layout.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useStaticQuery, graphql } from 'gatsby';
 
-// ====
+import Header from '../Header';
 
-const Layout = ({ children }) => <div>{children}</div>;
+//
+
+const Layout = ({ children }) => {
+  const siteMeta = useStaticQuery(graphql`
+    query siteMeta {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
+  return (
+    <>
+      <Header title={siteMeta.site.siteMetadata.title} />
+      <main role="main">{children}</main>
+    </>
+  );
+};
 
 export default Layout;
 
-// ====
+//
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
