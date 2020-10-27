387ca2a329757f981bbe4eb94f771ee1917e8755
Chrish Dunne <hey@chrish.design>
2020-10-27 16:32:02 +0000

    [+] Added video placeholder

---
+++ web/src/components/Video/Video.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+//
+
+const Video = ({ videoData }) => <div />;
+
+export default Video;
+
+//
+
+Video.propTypes = {
+  videoData: PropTypes.string.isRequired,
+};
