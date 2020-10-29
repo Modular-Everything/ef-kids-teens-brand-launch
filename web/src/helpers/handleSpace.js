741a380f62efe367b731d27c3e9c97a9c58e7e5c
Chrish Dunne <hey@chrish.design>
2020-10-29 13:19:00 +0000

    [~] Fix returning 0px values

--- web/src/helpers/handleSpace.js
+++ web/src/helpers/handleSpace.js
@@ -1,3 +1,12 @@
 export default function handleSpace(spacing) {
+  const topPad = spacing[0];
+  const bottomPad = spacing[1];
+
+  if (topPad + bottomPad === 0) return null;
+
+  window.addEventListener('resize', () => {
+    console.log('resized');
+  });
+
   return { paddingTop: `${spacing[0]}px`, paddingBottom: `${spacing[1]}px` };
 }
