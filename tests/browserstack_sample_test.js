dc2153f82c404e7dfb09c2a8300bc01abd7c4051
Chrish Dunne <hey@chrish.design>
2020-11-19 11:25:00 +0000

    [+] Added test test

---
+++ tests/browserstack_sample_test.js
@@ -0,0 +1,29 @@
+const webdriver = require('selenium-webdriver');
+
+// Input capabilities
+const capabilities = {
+  device: 'iPhone 11',
+  realMobile: 'true',
+  os_version: '14.0',
+  browserName: 'iPhone',
+  name: 'BStack-[NodeJS] Sample Test', // test name
+  build: 'BStack Build Number 1', // CI/CD job or build name
+  'browserstack.user': 'chrishdunne2',
+  'browserstack.key': 'ogpq6eRvhtio7QLpWsCs',
+};
+
+const driver = new webdriver.Builder()
+  .usingServer('https://hub-cloud.browserstack.com/wd/hub')
+  .withCapabilities(capabilities)
+  .build();
+driver.get('https://www.google.com').then(function () {
+  driver
+    .findElement(webdriver.By.name('q'))
+    .sendKeys('BrowserStack')
+    .then(function () {
+      driver.getTitle().then(function (title) {
+        console.log(title);
+        driver.quit();
+      });
+    });
+});
